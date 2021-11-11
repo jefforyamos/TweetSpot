95b88000b90a11d8fb2ec309174be8de295198dc
jeward <jeward@jackhenry.com>
2021-11-11 16:13:07 -0600

    Refactoring/improving unit tests.  Added one more cycle.

--- tests/TweetSpot.Primatives.xunit/ServiceBus/Events/FeedSpeedReportedTesting.cs
+++ tests/TweetSpot.Primatives.xunit/ServiceBus/Events/FeedSpeedReportedTesting.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
 using Xunit;
@@ -10,109 +11,119 @@ namespace TweetSpot.ServiceBus.Events
     {
         private readonly ITestOutputHelper _testOutput;
 
-
         // 
         // Timeline:
         //
-        // | <------------------------------ 15 seconds ----------------------------------------> |
-        // | <------------------------------ 700 tweets ----------------------------------------> |
-        // | <------------------------------ 46.7 / sec ----------------------------------------> |
-        // |       CYCLE ONE               |                CYCLE TWO                             |
-        // | ----------------------------- | ---------------------------------------------------- | 
-        // | <-----  5 seconds ----------> | <-------------------- 10 seconds ------------------> |
-        // | <------ 300 Tweets ---------> | <-------------------- 400 Tweets ------------------> |
-        // | <------- 60 / sec ----------> | <--------------------- 40 / sec -------------------> |
+        // |       CYCLE ONE               |                CYCLE TWO                             |         CYCLE THREE        |
+        // | ----------------------------- | ---------------------------------------------------- | -------------------------- |
+        // | <-----  5 seconds ----------> | <-------------------- 10 seconds ------------------> | <-----  4 seconds -------> |
+        // | <------ 300 Tweets ---------> | <-------------------- 400 Tweets ------------------> | <------ 300 Tweets ------> |
+        // | <------- 60 / sec ----------> | <--------------------- 40 / sec -------------------> | <------- 75 / sec -------> |
+        
+        // | <-------------------------------- 15 seconds --------------------------------------> |
+        // | <-------------------------------- 700 tweets --------------------------------------> |
+        // | <-------------------------------- 46.7 / sec --------------------------------------> |
+
+        // | <--------------------------------------------------- 19 seconds ------------------------------------------------> |
+        // | <-------------------------------------------------- 1000 tweets ------------------------------------------------> |
+        // | <-------------------------------------------------- 52.6 / sec -------------------------------------------------> |
         // 
 
-        private readonly int[] _secondDurationPerCycle = new[] { 5, 10 };
-        private readonly int[] _tweetCountPerCycle = new [] { 300, 400 };
+        private readonly int[] _secondDurationPerCycle = new[] { 5, 10 , 4};
+        private readonly int[] _tweetCountPerCycle = new [] { 300, 400, 300 };
         private readonly TimeSpan[] _elapsedTimePerCycle;
 
-
-        private readonly FeedSpeedReported _reportOne;
-        private readonly FeedSpeedReported _reportTwo;
+        private readonly FeedSpeedReported[] _report;
 
 
         public FeedSpeedReportedTesting(ITestOutputHelper testOutput)
         {
             _testOutput = testOutput;
-            _elapsedTimePerCycle = new[]
+            var elapsedList = new List<TimeSpan>();
+            for (var x = 0; x < _secondDurationPerCycle.Length; x++)
+                elapsedList.Add(TimeSpan.FromSeconds(_secondDurationPerCycle.Take(x + 1).Sum()));
+            _elapsedTimePerCycle = elapsedList.ToArray();
+            var reportList = new List<FeedSpeedReported>();
+            reportList.Add(new FeedSpeedReported(_elapsedTimePerCycle[0], (ulong) _tweetCountPerCycle[0], null));
+            for (int x = 1; x < _secondDurationPerCycle.Length; x++)
             {
-                TimeSpan.FromSeconds(_secondDurationPerCycle[0]),
-                TimeSpan.FromSeconds(_secondDurationPerCycle.Take(2).Sum()),
-            };
-            _reportOne = new FeedSpeedReported(_elapsedTimePerCycle[0], (ulong) _tweetCountPerCycle[0], null);
-            _reportTwo = new FeedSpeedReported(_elapsedTimePerCycle[1], (ulong) _tweetCountPerCycle.Take(2).Sum(), _reportOne);
+                var rpt = new FeedSpeedReported(_elapsedTimePerCycle[x], (ulong)_tweetCountPerCycle.Take(x+1).Sum(), reportList[x-1]);
+                reportList.Add(rpt);
+            }
+            _report = reportList.ToArray();
         }
-        [Fact]
-        public void TimeSpanForAllCycles_One_Correct()
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        public void TimeSpanForAllCycles_Any_Correct(int cycleNumber)
         {
-            Assert.Equal(_elapsedTimePerCycle[0], _reportOne.TimeSpanForAllCycles );
+            _testOutput.WriteLine(_report[cycleNumber].TimeSpanForAllCycles.ToString());
+            Assert.Equal(_elapsedTimePerCycle[cycleNumber], _report[cycleNumber].TimeSpanForAllCycles );
+        }
+
+        
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        public void TweetsReceivedForAllCycles_Any_Correct(int cycleNumber)
+        {
+            _testOutput.WriteLine(_report[cycleNumber].TweetsReceivedForAllCycles.ToString());
+            Assert.Equal((ulong) _tweetCountPerCycle.Take(cycleNumber+1).Sum(), _report[cycleNumber].TweetsReceivedForAllCycles);
         }
 
         [Fact]
-        public void TimeSpanForAllCycles_Two_Correct()
+        public void AverageSpeedTweetsPerSecond_FirstCycle_Correct()
         {
-            Assert.Equal(_elapsedTimePerCycle[1], _reportTwo.TimeSpanForAllCycles);
-        }
-
-        [Fact]
-        public void TweetsReceivedForAllCycles_One_Correct()
-        {
-            Assert.Equal((ulong) _tweetCountPerCycle[0], _reportOne.TweetsReceivedForAllCycles);
-        }
-
-        [Fact]
-        public void TweetsReceivedForAllCycles_Two_Correct()
-        {
-            Assert.Equal((ulong) _tweetCountPerCycle.Take(2).Sum(), _reportTwo.TweetsReceivedForAllCycles);
-        }
-
-        [Fact]
-        public void AverageSpeedTweetsPerSecond_One_Correct()
-        {
+            _testOutput.WriteLine(_report[0].AverageSpeedTweetsPerSecond.ToString("N1"));
             var tweets = Convert.ToDouble(_tweetCountPerCycle[0]);
             var seconds = Convert.ToDouble(_secondDurationPerCycle[0]);
-            Assert.Equal(tweets/seconds, _reportOne.AverageSpeedTweetsPerSecond);
+            Assert.Equal(tweets/seconds, _report[0].AverageSpeedTweetsPerSecond);
         }
 
         [Fact]
-        public void AverageSpeedTweetsPerSecond_One_SameAsCurrent()
+        public void AverageSpeedTweetsPerSecond_FirstCycle_SameAsCurrent()
         {
+            _testOutput.WriteLine(_report[0].AverageSpeedTweetsPerSecond.ToString("N1"));
             // On the first cycle, average speed is same as current speed because there is only one cycle to report
-            Assert.Equal(_reportOne.CurrentSpeedTweetsPerSecond , _reportOne.AverageSpeedTweetsPerSecond);
+            Assert.Equal(_report[0].CurrentSpeedTweetsPerSecond , _report[0].AverageSpeedTweetsPerSecond);
         }
 
-        [Fact]
-        public void AverageSpeedTweetsPerSecond_Two_VariesFromCurrent()
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        public void AverageSpeedTweetsPerSecond_NormalCycle_VariesFromCurrent(int cycleNumber)
         {
             // By the second cycle, the Average has diverged from the current
-            Assert.NotEqual(_reportTwo.CurrentSpeedTweetsPerSecond, _reportTwo.AverageSpeedTweetsPerSecond);
+            _testOutput.WriteLine(_report[cycleNumber].AverageSpeedTweetsPerSecond.ToString("N1"));
+            Assert.NotEqual(_report[cycleNumber].CurrentSpeedTweetsPerSecond, _report[cycleNumber].AverageSpeedTweetsPerSecond);
         }
 
-        [Fact]
-        public void AverageSpeedTweetsPerSecond_Two_Correct()
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        public void AverageSpeedTweetsPerSecond_NormalCycle_Correct(int cycleNumber)
         {
-            var tweets = Convert.ToDouble(_tweetCountPerCycle.Take(2).Sum());
-            var seconds = Convert.ToDouble(_secondDurationPerCycle.Take(2).Sum());
-            Assert.Equal(tweets / seconds, _reportTwo.AverageSpeedTweetsPerSecond);
+            _testOutput.WriteLine(_report[cycleNumber].AverageSpeedTweetsPerSecond.ToString("N1"));
+            var tweets = Convert.ToDouble(_tweetCountPerCycle.Take(cycleNumber+1).Sum());
+            var seconds = Convert.ToDouble(_secondDurationPerCycle.Take(cycleNumber+1).Sum());
+            Assert.Equal(tweets / seconds, _report[cycleNumber].AverageSpeedTweetsPerSecond);
         }
 
-        [Fact]
-        public void CurrentSpeedTweetsPerSecond_One_Correct()
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        public void CurrentSpeedTweetsPerSecond_Any_Correct(int cycleNumber)
         {
-            var tweets = Convert.ToDouble(_tweetCountPerCycle[0]);
-            var seconds = Convert.ToDouble(_secondDurationPerCycle[0]);
-            Assert.Equal(tweets / seconds, _reportOne.CurrentSpeedTweetsPerSecond);
+            _testOutput.WriteLine(_report[cycleNumber].CurrentSpeedTweetsPerSecond.ToString("N1"));
+            var tweets = Convert.ToDouble(_tweetCountPerCycle[cycleNumber]);
+            var seconds = Convert.ToDouble(_secondDurationPerCycle[cycleNumber]);
+            Assert.Equal(tweets / seconds, _report[cycleNumber].CurrentSpeedTweetsPerSecond);
         }
 
-        [Fact]
-        public void CurrentSpeedTweetsPerSecond_Two_Correct()
-        {
-            var tweets = Convert.ToDouble(_tweetCountPerCycle[1]);
-            var seconds = Convert.ToDouble(_secondDurationPerCycle[1]);
-            Assert.Equal(tweets / seconds, _reportTwo.CurrentSpeedTweetsPerSecond);
-        }
 
     }
 }
